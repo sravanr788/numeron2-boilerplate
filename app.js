dcfea363121713ad27d2b9e272c2adf6a928cf0d
sravanr788 <sravanr7882gmail.com>
2023-11-02 15:12:08 +0530

    numeron2

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // Iteration 1: Making the play button in the index.html functional.
 // Description: When the play button is clicked the game.html page should be opened
+var playbtn = document.getElementById("play-button")
+playbtn.onclick = () => {
+   window.location.href = "./game.html";
+}
--- score.js
+++ score.js
@@ -1,1 +1,17 @@
 // Iteration 8: Making scoreboard functional
+
+let scoreboard = document.getElementById("score-board")
+
+console.log(window.location.search)
+
+let params = new URLSearchParams(document.location.search);
+
+let score = params.get("score")
+console.log("score:", score)
+
+scoreboard.textContent = score;
+
+let playagainbtn = document.getElementById("play-again-button")
+playagainbtn.onclick = () => {
+    window.location.href = "./game.html";
+}
